31d3c0881c66e475f3f4c39f37f0c50df03c2860
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-07-09 14:25:57 +0700

    adding navbar and card

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,19 @@
-import Card from "../components/Cards";
+
 import React, { useState } from 'react'
 import jwtDecode from "jwt-decode";
 import axios from "axios";
-import { Button } from "@chakra-ui/react"
+import {
+    Button,
+    Stack,
+    Flex
+} from "@chakra-ui/react"
+import Card from "../components/Card";
+import Nav from "../components/Navbar";
+
 function Dashboard() {
     const [name, setName] = useState('')
-
     const getUserCredential = async () => {
-        const user = await axios.get('http://localhost:3001/api/token', {withCredentials: true})
+        const user = await axios.get('http://localhost:3001/api/token', { withCredentials: true })
         const token = user.data.accessToken
         const decoded = jwtDecode(token)
 
@@ -16,15 +22,8 @@ function Dashboard() {
 
     return (
         <>
-        <Button
-        onClick={() => {
-            getUserCredential()
-        }}
-        >
-
-            TES!
-        </Button>
-            <Card name={name}/>
+            <Nav />
+            <Card />
         </>
     )
 }
