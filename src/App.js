b091795d7fff22fbfb31a04d3d151a7221e8636e
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-07-09 14:36:57 +0700

    add new routes for message page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Routes, Route, BrowserRouter } from "react-router-dom"
 import Dashboard from "./pages/Dashboard";
+import Message from "./pages/Message"
 import Login from "./pages/Login"
 
 function App(){
@@ -10,6 +11,8 @@ function App(){
       <Route path="/" element={<Login />}>
       </Route>
       <Route path="/dashboard" element={<Dashboard />}>
+      </Route>
+      <Route path="/message/:user" element={<Message />}>
       </Route>
     </Routes>
     </BrowserRouter>
