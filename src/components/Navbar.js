cc00605e8cb02b3d1dae34f9fd40a0727e54c9fa
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-07-29 17:52:11 +0700

    fix typo on logout url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export default function Nav() {
   const logout = async () => {
     try {
 
-      await axios.delete(`${process.env.REACT_APP_BACKEND_URL}logout`, { withCredentials: true })
+      await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/logout`, { withCredentials: true })
       customToast('success', 'Berhasil logout...')
       setTimeout(() => {
         navigate("/")
